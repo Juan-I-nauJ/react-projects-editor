1e7c88ff2385e027f9d0a5aad7603b5b13f04d3c
Juan I. <juanpalaciagar@gmail.com>
2025-12-24 12:06:39 +0100

    feat: sidebar and noproject components added, placeholders removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import NoProject from "./components/NoProject/NoProject";
+import Sidebar from "./components/Sidebar/Sidebar";
+
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <Sidebar />
+      <NoProject />
     </>
   );
 }
