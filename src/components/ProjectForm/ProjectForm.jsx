3007707e9fdc57ce5d14bebbe6fdb86b6966b5b8
Juan I. <juanpalaciagar@gmail.com>
2025-12-24 15:06:17 +0100

    feat: form logic finished

--- src/components/ProjectForm/ProjectForm.jsx
+++ src/components/ProjectForm/ProjectForm.jsx
@@ -1,4 +1,11 @@
+import { useRef } from "react"
+
+
 export default function ProjectForm({cancelProject, addProject}) {
+    const titleValues = useRef()
+    const descValues = useRef()
+    const dateValues = useRef()
+    
     return (
         <form className="w-4xl">
             <legend>
@@ -8,20 +15,20 @@ export default function ProjectForm({cancelProject, addProject}) {
             </legend>
             <menu className="flex gap-4 justify-end">
                 <button type="button" onClick={() => cancelProject(false)} className="cursor-pointer px-4 py-2 text-xs md:text-base rounded-md text-stone-400 hover:text-stone-800 w-16">Cancel</button>
-                <button type="button" onClick={() => addProject({title: 'here we are', desc: 'born to be kings', date: 'we are the princes of the universe'})} className="cursor-pointer px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-700 hover:text-stone-100 w-16">Save</button>
+                <button type="button" onClick={() => addProject({title: titleValues.current.value, desc: descValues.current.value, date: dateValues.current.value})} className="cursor-pointer px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-700 hover:text-stone-100 w-16">Save</button>
             </menu>
             <fieldset className="flex flex-col gap-8">
                 <div className="text-left">
                     <label htmlFor="title-field" className="text-sm font-bold uppercase text-stone-500">Title</label>
-                    <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" id="title-field" name="title-field" type="text" />
+                    <input ref={titleValues} required className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" id="title-field" name="title-field" type="text" />
                 </div>
                 <div className="text-left">
                     <label htmlFor="desc-field" className="text-sm font-bold uppercase text-stone-500">Description</label>
-                    <textarea className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" name="desc-field" id="desc-field"></textarea>
+                    <textarea ref={descValues} required className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" name="desc-field" id="desc-field"></textarea>
                 </div>
                 <div className="text-left">
                     <label htmlFor="date-field" className="text-sm font-bold uppercase text-stone-500">Due date</label>
-                    <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" type="date" name="date-field" id="date-field" />
+                    <input ref={dateValues} required className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" type="date" name="date-field" id="date-field" />
                 </div>
             </fieldset>
         </form>
